5b38c0a5cae51db149f5c9d0740702d2fc8cf60c
i3Ammar <aaabughaith21@cit.just.edu.jo>
2025-02-25 01:45:07 +0300

    finish omdb api

--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -9,117 +9,157 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
 
+from configurations import Configuration
+from configurations import values
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+class Dev(Configuration):
+        # Build paths inside the project like this: BASE_DIR / 'subdir'.
+        BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+        # Quick-start development settings - unsuitable for production
+        # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zaiqyyg8b%h58-f)dga!6sgs$tkk1qi*m_$gv=w=%_3)pdj@e@'
+        # SECURITY WARNING: keep the secret key used in production secret!
+        SECRET_KEY = 'django-insecure-zaiqyyg8b%h58-f)dga!6sgs$tkk1qi*m_$gv=w=%_3)pdj@e@'
+        # OMDB_KEY = values.SecretValue()
+        OMDB_KEY = "cdb7d6b4"
+        # the key used is = 'cdb7d6b4'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+        # SECURITY WARNING: don't run with debug turned on in production!
+        DEBUG = True
 
-ALLOWED_HOSTS = []
+        ALLOWED_HOSTS = ['127.0.0.1', 'localhost','diddy']
 
-
-# Application definition
-
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-]
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-ROOT_URLCONF = 'course4_proj.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-WSGI_APPLICATION = 'course4_proj.wsgi.application'
+        # X_FRAME_OPTIONS = "ALLOW-FROM " + os.environ.get("CODIO_HOSTNAME") + "-8000.codio.io"
+        CSRF_COOKIE_SAMESITE = None
+        # CSRF_TRUSTED_ORIGINS = [os.environ.get("CODIO_HOSTNAME") + "-8000.codio.io"]
+        CSRF_COOKIE_SECURE = True
+        SESSION_COOKIE_SECURE = True
+        CSRF_COOKIE_SAMESITE = "None"
+        SESSION_COOKIE_SAMESITE = "None"
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+        # Application definition
+        LOCAL_APPS = [
+            'movies',
+                      ]
+        INSTALLED_APPS = [
+            'django.contrib.admin',
+            'django.contrib.auth',
+            'django.contrib.contenttypes',
+            'django.contrib.sessions',
+            'django.contrib.messages',
+            'django.contrib.staticfiles',
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
-# Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
+        ]+LOCAL_APPS
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/3.2/topics/i18n/
+        MIDDLEWARE = [
+            'django.middleware.security.SecurityMiddleware',
+            'django.contrib.sessions.middleware.SessionMiddleware',
+            'django.middleware.common.CommonMiddleware',
+            # 'django.middleware.csrf.CsrfViewMiddleware',
+            'django.contrib.auth.middleware.AuthenticationMiddleware',
+            'django.contrib.messages.middleware.MessageMiddleware',
+            # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+        ]
 
-LANGUAGE_CODE = 'en-us'
+        ROOT_URLCONF = 'course4_proj.urls'
 
-TIME_ZONE = 'UTC'
+        TEMPLATES = [
+            {
+                'BACKEND': 'django.template.backends.django.DjangoTemplates',
+                'DIRS': [],
+                'APP_DIRS': True,
+                'OPTIONS': {
+                    'context_processors': [
+                        'django.template.context_processors.debug',
+                        'django.template.context_processors.request',
+                        'django.contrib.auth.context_processors.auth',
+                        'django.contrib.messages.context_processors.messages',
+                    ],
+                },
+            },
+        ]
 
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
+        WSGI_APPLICATION = 'course4_proj.wsgi.application'
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
+        # Database
+        # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-STATIC_URL = '/static/'
+        DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': BASE_DIR / 'db.sqlite3',
+            }
+        }
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+        # Password validation
+        # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+
+        AUTH_PASSWORD_VALIDATORS = [
+            {
+                'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+            },
+            {
+                'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+            },
+            {
+                'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+            },
+            {
+                'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+            },
+        ]
+
+
+        # Internationalization
+        # https://docs.djangoproject.com/en/3.2/topics/i18n/
+
+        LANGUAGE_CODE = 'en-us'
+
+        TIME_ZONE = 'UTC'
+
+        USE_I18N = True
+
+        USE_L10N = True
+
+        USE_TZ = True
+
+
+        # Static files (CSS, JavaScript, Images)
+        # https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+        STATIC_URL = '/static/'
+
+        # Default primary key field type
+        # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+        DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+        LOGGING = {
+            "version": 1,
+            "disable_existing_loggers": False,
+            "formatters": {
+                "verbose": {
+                    "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+                    "style": "{",
+                },
+            },
+            "handlers": {
+                "console": {
+                    "class": "logging.StreamHandler",
+                    "stream": "ext://sys.stdout",
+                    "formatter": "verbose",
+                }
+            },
+            "root": {
+                "handlers": ["console"],
+                "level": "DEBUG",
+            },
+        }
